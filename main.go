361f42a6b213a8ca96595572e4160d11cfb9d4b8
zulkou <73563319+zulkou@users.noreply.github.com>
2025-03-18 14:57:55 +0700

    feat: add executable command

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
 	"github.com/zulkou/blog-aggregator/internal/config"
 )
@@ -12,15 +14,71 @@ func main() {
         fmt.Println(err)
     }
 
-    err = cfg.SetUser("zulkou")
-    if err != nil {
-        fmt.Println(err)
+    s := state {
+        cfg: &cfg,
+    }
+    cmds := commands{
+        handlers: make(map[string]func(*state, command) error),
     }
 
-    cfg, err = config.Read()
-    if err != nil {
-        fmt.Println(err)
+    cmds.register("login", handlerLogin)
+
+    args := os.Args
+
+    if len(args) < 2 {
+        fmt.Println("require command name")
+        os.Exit(1)
+    }
+    
+    cmd := command{
+        name: args[1],
+        args: args[2:],
     }
 
-    fmt.Printf("db_url: \"%v\"\ncurrent_user_name: \"%v\"", cfg.DBURL, cfg.CurrentUserName)
+    err = cmds.run(&s, cmd)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
 }
+
+type state struct {
+    cfg     *config.Config
+}
+
+type command struct {
+    name    string
+    args    []string
+}
+
+type commands struct {
+    handlers    map[string]func(s *state, cmd command) error
+}
+
+func handlerLogin(s *state, cmd command) error {
+    if len(cmd.args) == 0 || len(cmd.args) > 1 {
+        return errors.New("only accepts one argument")
+    }
+
+    err := s.cfg.SetUser(cmd.args[0])
+    if err != nil {
+        return fmt.Errorf("user not found: %w", err)
+    }
+
+    fmt.Printf("State assigned to %s, Welcome!\n", s.cfg.CurrentUserName)
+    return nil
+}
+
+func (c *commands) register(name string, f func(s *state, cmd command) error) {
+    c.handlers[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+    handler, exists := c.handlers[cmd.name]
+    if !exists {
+        return fmt.Errorf("command not found: %s", cmd.name)
+    }
+
+    return handler(s, cmd)
+}
+
